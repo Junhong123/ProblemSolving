cd06b49c65d99ef23900bef4a7aafd8e0e93cc1e
Junhong <pparksj22@gmail.com>
2025-11-24 21:31:24 +0900

    Implement

--- BAEKJOON/Implement/2775/2775.cpp
+++ BAEKJOON/Implement/2775/2775.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int T;
     cin >> T;
+
+    while (T--)
+    {
+        int k, n;
+        cin >> k >> n;
+        vector<vector<int>> arr(k + 1, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+            arr[0][i] = i + 1;
+        for (int i = 1; i <= k; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k <= j; k++)
+                    arr[i][j] += arr[i - 1][k];
+
+        cout << arr[k][n - 1] << "\n";
+    }
 
     return 0;
 }
