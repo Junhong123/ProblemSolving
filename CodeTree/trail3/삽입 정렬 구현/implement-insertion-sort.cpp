1c8b16a248af8a7eb2d4e422339853a8733f7521
Junhong123 <81297679+Junhong123@users.noreply.github.com>
2026-06-06 22:40:46 +0900

    [Passed] 삽입 정렬 구현 (2ms, 0MB)

---
+++ CodeTree/trail3/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+    sort(arr.begin(),arr.end());
+    for(auto &v : arr) cout << v << " ";
+    return 0;
+}
