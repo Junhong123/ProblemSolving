ce8bf925efe3bb318a4bd7847eef1252e36312f7
Junhong123 <81297679+Junhong123@users.noreply.github.com>
2026-06-12 23:20:57 +0900

    [Passed] 병합 정렬 구현 (34ms, 1MB)

---
+++ CodeTree/trail3/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    for(auto& v: arr) cout << v << " ";
+    return 0;
+}
